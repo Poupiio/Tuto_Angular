7ffd088c67f557a8c639de392657970176a891b1
Elise <elise.maaiza33@gmail.com>
2024-05-02 16:35:41 +0200

    🚧 First event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,18 @@ import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Liste de Pokémons</h1>`
+  templateUrl: "app.component.html",
 })
 export class AppComponent implements OnInit {
   pokemonList: Pokemon[] = POKEMONS;
 
   ngOnInit() {
     console.table(this.pokemonList);
-    this.selectPokemon(this.pokemonList[0]);
   }
 
-  selectPokemon(pokemon: Pokemon) {
-    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+  selectPokemon(event: MouseEvent) {
+    // .value par défaut renvoie une string, je dois donc le convertir en nombre. J'utilise l'opérateur + pour transformer ce qui se trouve à droite en un nombre
+    const index: number = +(event.target as HTMLInputElement).value;
+    console.log(`Vous avez cliqué sur le pokémon ${this.pokemonList[index].name}`);
   }
 }
