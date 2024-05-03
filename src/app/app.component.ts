1fa562974695ab33fe03c1d57c6784c1943c6b63
Elise <elise.maaiza33@gmail.com>
2024-05-03 11:56:59 +0200

    💄 Using Materialize CSS library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent implements OnInit {
   pokemonSelected?: Pokemon;
 
   ngOnInit() {
-    console.table(this.pokemonList);
+    // console.table(this.pokemonList);
   }
 
   selectPokemon(pokemonId: string) {
