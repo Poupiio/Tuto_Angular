57ce35b730b2c9b889159bc95a8e0d03edf5c2d8
Elise <elise.maaiza33@gmail.com>
2024-05-06 13:32:37 +0200

    🚧 Routing ok

---
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { POKEMONS } from '../mock-pokemon-list';
+import { Pokemon } from '../pokemon';
+
+@Component({
+  selector: 'app-detail-pokemon',
+  templateUrl: './detail-pokemon.component.html',
+  styles: [
+  ]
+})
+export class DetailPokemonComponent implements OnInit {
+  pokemonList: Pokemon[] = [];
+  pokemon?: Pokemon;
+
+  constructor(private route: ActivatedRoute, private router: Router) { }
+
+  goBack() {
+    this.router.navigate(["pokemons"]);
+  }
+
+  ngOnInit() {
+    this.pokemonList = POKEMONS;
+    const pokemonId: string | null = this.route.snapshot.paramMap.get('id');
+
+    if (pokemonId) {
+      this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
+    }
+  }
+
+}
